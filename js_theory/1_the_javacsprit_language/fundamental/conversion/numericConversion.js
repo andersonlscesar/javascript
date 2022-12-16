f2261d277c3d9e18f8d511370f154796521920a0
andersonlscesar <andersonlscesar@gmail.com>
2022-12-16 09:05:00 -0300

    numeric conversion

---
+++ js_theory/1_the_javacsprit_language/fundamental/conversion/numericConversion.js
@@ -0,0 +1,8 @@
+let number = '123';
+number = Number(number);
+console.log(typeof number); // number
+
+// Boolean type
+
+console.log( Boolean("")); // false 
+console.log( Boolean(" ")); // true
---
+++ js_theory/1_the_javacsprit_language/fundamental/conversion/stringConversion.js
@@ -0,0 +1,5 @@
+let value = true;
+console.log(typeof value); // boolean
+value = String(value); // now this value is a string
+console.log(typeof value); // string
+
